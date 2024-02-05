e8e7aa06ddf419ae43da9061df68056210037d7b
Fabio Fleck <ffleck@gmail.com>
2024-02-05 16:35:14 -0300

    atualizando login

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://karavel-api-1226bbda6c0b.herokuapp.com",
-  baseURL: "http://184.72.22.149:3333", // testes
+  baseURL: "http://localhst:3333", // testes
 });
 
 export default api;
