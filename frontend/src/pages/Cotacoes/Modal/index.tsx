5920e110d05877c7a5d3dfd26b47ab4a3477d09d
Fabio Fleck <fabio.fleck.external@lojasrenner.com.br>
2024-05-22 15:56:42 -0300

    Atualizando

--- frontend/src/pages/Cotacoes/Modal/index.tsx
+++ frontend/src/pages/Cotacoes/Modal/index.tsx
@@ -96,7 +96,7 @@ const LabelModal = (props: ResultadoProps) => {
                 </Popover>
               }
             >
-              <Button>USD {props.base_freight + props.bunker + props.isps} </Button>
+              <Button>USD {parseFloat(props.base_freight) + parseFloat(props.bunker) + parseFloat(props.isps)} </Button>
             </OverlayTrigger>
           ))}
         </>
