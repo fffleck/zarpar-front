f8857aa6fb005c80f6100521f64c7926d3fba4d0
Fabio Fleck <fabio.fleck.external@lojasrenner.com.br>
2024-07-09 20:22:03 -0300

    Ajustando recuperacao de senha

--- frontend/src/pages/ResetPassword/index.tsx
+++ frontend/src/pages/ResetPassword/index.tsx
@@ -44,146 +44,94 @@ const ResetPassword = () => {
   };
 
   return (
-    <div
-      className="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5"
-      role="dialog"
-      id="modalSignin"
-    >
-      <div style={{textAlign: "center"}} role="document">
-        <img src="imagens/logo_escura.png"  alt="" />
+    <div className="login-page">
+      <div className="image-container">
+        <img src="/imagens/logo_home_2.jpeg" alt="Login" className="login-image" />
       </div>
-      <div className="modal-dialog" role="document">
-        <div className="modal-content rounded-4 shadow">
-          <div className="modal-header p-5 pb-4 border-bottom-0">
-            <h4 className="card-title mb-4">Redefinir senha</h4>
-          </div>
+      <div className="form-container">
+        <div className="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" role="dialog" id="modalSignin">
+        <div style={{textAlign: "center"}} role="document">
+                <img src="/imagens/logo+white.png"  width="250px" height="70px" alt="" />
+              </div>
+          <div className="modal-dialog" role="document">
+            <div className="modal-content rounded-4 shadow">
+              <div className="modal-header p-5 pb-4 border-bottom-0">
+                <h4 className="card-title mb-4">Redefinir senha</h4>
+              </div>
 
-          <div className="modal-body p-5 pt-0">
-            <form onSubmit={handleSubmit}>
-              {showFields && (
-                <div>
-                  <div className="form-group">
-                    <label className="mb-2" htmlFor="password">
-                      Nova senha
-                    </label>
-                    <input
-                      className="form-control"
-                      type="password"
-                      name="password"
-                      value={password}
-                      onChange={handleInputChange}
-                      required
-                    />
-                  </div>
-                  <div className="reset-password-form-group">
-                    <label className="mb-2 mt-4" htmlFor="passwordConfirmation">
-                      Confirme a nova senha
-                    </label>
-                    <input
-                      className="form-control"
-                      type="password"
-                      name="passwordConfirmation"
-                      value={passwordConfirmation}
-                      onChange={handleInputChange}
-                      required
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="w-100 mb-2 btn btn-md rounded-3 btn-primary mt-4"
-                    id="botao-login"
-                  >
-                    Redefinir senha
-                  </button>
-                </div>
-              )}
-              {errorMessage && (
-                <div>
-                  <div className="alert alert-danger" role="alert">
-                    {errorMessage}
-                  </div>
-                  <div className="text-center mt-3">
-                    <Link to="/" className="text-decoration-none">
-                      Voltar para o login
-                    </Link>
-                  </div>
-                </div>
-              )}
-              {successMessage && (
-                <div>
-                  <div className="alert alert-success" role="alert">
-                    {successMessage}
-                  </div>
-                  <div className="text-center mt-3">
-                    <Link to="/" className="text-decoration-none">
-                      Voltar para o login
-                    </Link>
-                  </div>
-                </div>
-              )}
-            </form>
+              <div className="modal-body p-5 pt-0">
+                <form onSubmit={handleSubmit}>
+                  {showFields && (
+                    <div>
+                      <div className="form-group">
+                        <label className="mb-2" htmlFor="password">
+                          Nova senha
+                        </label>
+                        <input
+                          className="form-control"
+                          type="password"
+                          name="password"
+                          value={password}
+                          onChange={handleInputChange}
+                          required
+                        />
+                      </div>
+                      <div className="reset-password-form-group">
+                        <label className="mb-2 mt-4" htmlFor="passwordConfirmation">
+                          Confirme a nova senha
+                        </label>
+                        <input
+                          className="form-control"
+                          type="password"
+                          name="passwordConfirmation"
+                          value={passwordConfirmation}
+                          onChange={handleInputChange}
+                          required
+                        />
+                      </div>
+                      <button
+                        type="submit"
+                        className="w-100 mb-2 btn btn-md rounded-3 btn-primary mt-4"
+                        id="botao-login"
+                      >
+                        Redefinir senha
+                      </button>
+                    </div>
+                  )}
+                  {errorMessage && (
+                    <div>
+                      <div className="alert alert-danger" role="alert">
+                        {errorMessage}
+                      </div>
+                      <div className="text-center mt-3">
+                        <Link to="/" className="text-decoration-none">
+                          Voltar para o login
+                        </Link>
+                      </div>
+                    </div>
+                  )}
+                  {successMessage && (
+                    <div>
+                      <div className="alert alert-success" role="alert">
+                        {successMessage}
+                      </div>
+                      <div className="text-center mt-3">
+                        <Link to="/" className="text-decoration-none">
+                          Voltar para o login
+                        </Link>
+                      </div>
+                    </div>
+                  )}
+                </form>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+      </div>
+    );
 
-  //   return (
-  //     <div className="reset-password">
-  //       <div className="reset-password-form">
-  //         <div className="reset-password-form-wrapper">
-  //           <div className="reset-password-title">
-  //             <h2>Redefinir senha</h2>
-  //           </div>
-  //           <form onSubmit={handleSubmit}>
-  //             <div className="reset-password-form-group">
-  //               <label className="reset-password-form-label" htmlFor="password">
-  //                 Nova senha
-  //               </label>
-  //               <input
-  //                 className="reset-password-form-input"
-  //                 type="password"
-  //                 name="password"
-  //                 value={password}
-  //                 onChange={handleInputChange}
-  //                 required
-  //               />
-  //             </div>
-  //             <div className="reset-password-form-group">
-  //               <label
-  //                 className="reset-password-form-label"
-  //                 htmlFor="passwordConfirmation"
-  //               >
-  //                 Confirme a nova senha
-  //               </label>
-  //               <input
-  //                 className="reset-password-form-input"
-  //                 type="password"
-  //                 name="passwordConfirmation"
-  //                 value={passwordConfirmation}
-  //                 onChange={handleInputChange}
-  //                 required
-  //               />
-  //             </div>
-  //             <button type="submit" className="btn btn-primary" id="botao-login">
-  //               Redefinir senha
-  //             </button>
-  //             {errorMessage && (
-  //               <div className="reset-password-form-error">{errorMessage}</div>
-  //             )}
-  //             {successMessage && (
-  //               <div className="reset-password-form-success">
-  //                 {successMessage}
-  //               </div>
-  //             )}
-  //           </form>
-  //         </div>
-  //       </div>
-  //       <div className="reset-password-banner">
-  //         <img src="../../imagens/proposito_reduzido.png" alt="" />
-  //       </div>
-  //     </div>
-  //   );
 };
 
 export default ResetPassword;
