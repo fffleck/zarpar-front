01ce743bdf802dd101b6f9d380b3f31e35f07c16
Fabio Fleck <fabio.fleck.external@lojasrenner.com.br>
2024-09-12 17:58:27 -0300

    Atualizando

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://http://3.129.16.6:3334", // testes
-  // baseURL: "http://localhost:3334", // testes
+  // baseURL: "http://3.129.16.6:3334", // testes
+  baseURL: "http://localhost:3334", // testes
 });
 
 export default api;
